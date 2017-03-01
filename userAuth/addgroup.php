929f604eb37c229b974804731efb9576aa2c9122
KhogaEslam <KhogaEslam@gmail.com>
2017-03-01 21:29:00 +0200

    updating database, changing groups> adding callBack field

--- userAuth/addgroup.php
+++ userAuth/addgroup.php
@@ -1,18 +1,19 @@
 <?php
-if (!isset($_POST['group_desc']) ) {
+if (!isset($_POST['group_name']) ) {
  ?>
  <html>
    <head>
-      <title> Add goods </title>
+      <title> Add Group </title>
       <script>
 
       </script>
    </head>
    <body>
      <form method='post' action="addgroup.php">
-        <input class="form-control" type="text" name="group_name" id="unitprice-id" placeholder="add group name">
-        <textarea class="form-control" name="group_desc" id="product-id" placeholder="add group description"></textarea>
-        <input  class="form-control" type="number" name="group_proj_num" id="saleQuantity-id" placeholder="add project number">
+        <input class="form-control" type="text" name="group_name" id="group_name" placeholder="add group name">
+        <textarea class="form-control" name="group_desc" id="group_desc" placeholder="add group description"></textarea>
+        <input class="form-control" type="text" name="callBack" id="callBack" placeholder="add callBack function url">
+        <input  class="form-control" type="number" name="group_proj_num" id="group_proj_num" placeholder="add project number">
         <input type="submit" value ="submit">
 
      </form>
@@ -23,8 +24,8 @@ if (!isset($_POST['group_desc']) ) {
  else {
    extract($_POST);
    include("dbconnect.php");
-   $sql_statement="insert into groups(name,group_desc, proj_num) values (\" $group_name \",
-    \"$group_desc\", \"$group_proj_num\");";
+   $sql_statement="insert into groups(name,group_desc,callBack, proj_num) values (\" $group_name \",
+    \"$group_desc\", \"$callBack\", \"$group_proj_num\");";
     echo $sql_statement;
     $result = mysqli_query( $db, $sql_statement);
     if (! $result ) {
--- userAuth/editgroup.php
+++ userAuth/editgroup.php
@@ -7,27 +7,31 @@
 		$result = mysqli_query( $db, $sql_statement);
 		if ($result){
 			$r = mysqli_fetch_assoc($result);
-			
+
 		if (! isset($_POST['group_name'])) {
 			?>
 			<form method = 'post'>
 				<label>
-					Group Name 
+					Group Name
 				</label>
-				<input name= 'group_name' type = "text" value = '<?= $r['name'] ?>' > 
+				<input name= 'group_name' type = "text" value = '<?= $r['name'] ?>' >
 				<label>
 					Group Description
 				</label>
 				<input name= 'group_desc' type = 'text' value = '<?= $r['group_desc'] ?>' >
 				<label>
-					Project Number 
+					Callback Function
+				</label>
+				<input name= 'callBack' type = 'text' value = '<?= $r['callBack'] ?>' >
+				<label>
+					Project Number
 				</label>
 				<input name= 'group_proj_num' type = 'text' value = '<?= $r['proj_num'] ?>' >
 				<input type="submit" value ="submit">
 			</form>
-		<?php } else { 
+		<?php } else {
 			extract($_POST);
-$sql_statement="update groups set name= '$group_name', group_desc='$group_desc' , proj_num = $group_proj_num where id = $id;"; 
+$sql_statement="update groups set name= '$group_name', group_desc='$group_desc' , callBack='$callBack' proj_num = $group_proj_num where id = $id;";
 		     mysqli_query( $db, $sql_statement);
 		     header('Location: index.php');
 
